38e463e21f72196e6873de65d309d118740c00cb
Alan-oliveir <alandeog@poli.ufrj.br>
2023-05-23 20:57:27 -0300

    add progress bar

--- main.py
+++ main.py
@@ -52,34 +52,30 @@ def search():
     MainFrame.l_views.configure(text="Views: " + str(info_video["Views"]))
     MainFrame.l_date.configure(text="Data: " + info_video["Date"])
     MainFrame.l_duration.configure(text="Duração: " + info_video["Duration"])
-    MainFrame.img_thumb.configure(text=None, image=info_video["Thumbnail"])
+    MainFrame.img_thumb.configure(text="", image=info_video["Thumbnail"])
     
     print (info_video)  
 
-'''print(cover)
+def download():
 
-previousprogress = 0
+    try:
+        ytLink = MainFrame.e_url.get()
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+        video = ytObject.streams.get_highest_resolution()
+        video.download()
+        FrameBottomBar.l_status.configure(text="Concluído", anchor="center")
+    
+    except:
+        FrameBottomBar.l_status.configure(text="Falhou", text_color="red")
+
 
 def on_progress(stream, chunk, bytes_remaining):
-    global previousprogress
+
     total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining 
+    bytes_downloaded = total_size - bytes_remaining
+    porcentage_completed = bytes_downloaded / total_size * 100
+    print(porcentage_completed)
 
-    liveprogress = (int)(bytes_downloaded / total_size * 100)
-    if liveprogress > previousprogress:
-        previousprogress = liveprogress
-        print(liveprogress)
-        bar.place(x=250, y=120)
-        bar['value'] = liveprogress
-        janela.update_idletasks()
-
-def download():
-    url=e_url.get()
-    yt=YouTube(url)
-
-    yt.register_on_progress_callback(on_progress)
-    yt.streams.filter(only_audio=False).first().download()
-'''
 
 class FrameTitleBar(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
@@ -124,7 +120,7 @@ class MainFrame(ctk.CTkFrame):
         MainFrame.img_thumb = ctk.CTkLabel(self, image=None, text="Cover", anchor=ctk.NW, width=575, height=325)  # display video thumb with a CTkLabel
         MainFrame.img_thumb.grid(row=2, column=0, rowspan=5, columnspan=2, padx=(20, 20), pady=(15, 30))
         
-        self.b_download = ctk.CTkButton(self, text="Baixar", width=140, command=None, font=font_title, height=50, image=img_download, compound="left")
+        self.b_download = ctk.CTkButton(self, text="Baixar", width=140, command=download, font=font_title, height=50, image=img_download, compound="left")
         self.b_download.grid(row=6, column=2, padx=(20, 20), pady=(15, 30))
 
         # Widgets with video data
@@ -144,6 +140,29 @@ class MainFrame(ctk.CTkFrame):
         MainFrame.l_duration.grid(row=5, column=2, padx=20, pady=15, stick="nw")
 
 
+class FrameBottomBar(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        
+        # Font object
+        font_label = ctk.CTkFont(size=14, weight="bold")
+        font_status = ctk.CTkFont(size=18, weight="bold")
+       
+        # Add widgets onto the FrameBottomBar
+        l_porcentage = ctk.CTkLabel(self, text="Progresso:", font=font_label)
+        l_porcentage.grid(row=0, column=0, padx=(10, 0), pady=5, stick="ne")
+
+        l_porcentage = ctk.CTkLabel(self, text="0%", font=font_label)
+        l_porcentage.grid(row=0, column=1, padx=(5, 5), pady=5, stick="ne")
+
+        progress = ctk.CTkProgressBar(self, width=560, height=12)
+        progress.set(0)
+        progress.grid(row=0, column=2, padx=(5, 0), pady=5, stick="e")
+
+        FrameBottomBar.l_status = ctk.CTkLabel(self, text="", font=font_status)
+        FrameBottomBar.l_status.grid(row=0, column=3, padx=20, pady=5)
+
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -156,11 +175,13 @@ class App(ctk.CTk):
 
         # Add frames to App        
         self.FrameTitleBar = FrameTitleBar(master=self)
-        self.FrameTitleBar.grid(row=0, column=0, padx=5, pady=5, stick="new")
+        self.FrameTitleBar.grid(row=0, column=0, padx=5, pady=(5, 2), stick="new")
 
-        self.frame = MainFrame(master=self)
-        self.frame.grid(row=1, column=0, padx=5, pady=(0, 8), sticky="nsew")
+        self.MainFrame = MainFrame(master=self)
+        self.MainFrame.grid(row=1, column=0, padx=5, pady=(0, 2), sticky="nsew")
 
+        self.FrameBottomBar = FrameBottomBar(master=self)
+        self.FrameBottomBar.grid(row=2, column=0, padx=5, pady=(0, 5), stick="nsew")
 
 app = App()
 app.mainloop()

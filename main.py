8a70d6d5f3b6beb0f4b7628ce78350a435787770
Alan-oliveir <alandeog@poli.ufrj.br>
2023-05-23 18:28:06 -0300

    update main.py and change app logo

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-from PIL import Image
-
 import os
+
+from PIL import Image
 
 import customtkinter as ctk
 
 import requests
-
+import datetime
 from pytube import YouTube
 
-import datetime
-from datetime import date
-import calendar
-# import downloader
+# =============== Constants ================
 
-# Constants
 PATH = os.path.dirname(os.path.realpath(__file__))
 
 # ================ Funtions ================
 
 def search():
     
-    global thumbnail, info_video
+    global info_video
 
     # Get Youtube video url
     url = MainFrame.e_url.get()
@@ -38,7 +34,7 @@ def search():
     # Get and processing of thumbnail
     thumbnail_url = yt.thumbnail_url # cover of the video
     thumbnail = Image.open(requests.get(thumbnail_url, stream=True).raw)
-    thumbnail = ctk.CTkImage(light_image=thumbnail, size=(280, 200))
+    thumbnail = ctk.CTkImage(light_image=thumbnail, size=(570, 320))
 
     info_video = {
         'Title': title,
@@ -46,28 +42,21 @@ def search():
         'Views': number_views,
         'Rating': rating,
         'Duration': duration,
-        'Date': publish_date,        
+        'Date': publish_date,
+        'Thumbnail': thumbnail,        
         'Description': description
     }
 
     MainFrame.l_title.configure(text="Título: " + info_video["Title"])
+    MainFrame.l_channel.configure(text="Canal: " + info_video["Channel"])
+    MainFrame.l_views.configure(text="Views: " + str(info_video["Views"]))
+    MainFrame.l_date.configure(text="Data: " + info_video["Date"])
+    MainFrame.l_duration.configure(text="Duração: " + info_video["Duration"])
+    MainFrame.img_thumb.configure(text=None, image=info_video["Thumbnail"])
     
-    #print (info_video)
+    print (info_video)  
 
-    #return (info_video)
-    '''img_ = img_.resize((230, 150), Image.ANTIALIAS)
-    img_ = ImageTk.PhotoImage(img_)
-
-    global img
-
-    img=img_
-    l_image['image']=img
-
-    l_title['text']="Titlo : " + str(title)
-    l_view['text']="Views : " + str('{:,}'.format(view))
-    l_time['text']="Duracao : " + str(duration)
-
-print(cover)
+'''print(cover)
 
 previousprogress = 0
 
@@ -90,8 +79,6 @@ def download():
 
     yt.register_on_progress_callback(on_progress)
     yt.streams.filter(only_audio=False).first().download()
-
-
 '''
 
 class FrameTitleBar(ctk.CTkFrame):
@@ -102,7 +89,7 @@ class FrameTitleBar(ctk.CTkFrame):
         font_title = ctk.CTkFont(family="Verdana", size=21, weight="bold")
 
         # Open image
-        app_img = ctk.CTkImage(Image.open(PATH + "/images/logo.png"), size=(60,45))
+        app_img = ctk.CTkImage(Image.open(PATH + "/images/app_logo.png"), size=(60,45))
         
         # Add widgets onto the FrameTitleBar
         label_img = ctk.CTkLabel(self, text="", image=app_img)
@@ -111,49 +98,60 @@ class FrameTitleBar(ctk.CTkFrame):
         label_title = ctk.CTkLabel(self, text="Youtube Downloader App", font=font_title)
         label_title.grid(row=0, column=1, padx=(5,0), pady=5, stick="nse")
 
+
 class MainFrame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-       
-        my_image = ctk.CTkImage(light_image=Image.open("D:\Arquivos\GitHub\youtube_downloader\img-pokemon-list.png"), size=(375, 320))
-
+               
+        # Open image
         img_download = ctk.CTkImage(Image.open(PATH + "/images/download.png"), size=(25, 25))
-
         img_search = ctk.CTkImage(Image.open(PATH + "/images/search.png"), size=(25, 25))
 
+        # Font object
         font_title = ctk.CTkFont(size=14, weight="bold")
-        font_label = ctk.CTkFont(size=13, weight="bold")
+        font_label = ctk.CTkFont(size=13, weight="normal")
 
-        # add widgets onto the frame        
-        self.l_url = ctk.CTkLabel(self, text="Enter URL:", font=font_title) # label url
+        # Add widgets onto the MainFrame        
+        self.l_url = ctk.CTkLabel(self, text="Enter URL:", font=font_title)
         self.l_url.grid(row=0, column=0, padx=20, pady=15)
-        MainFrame.e_url = ctk.CTkEntry(self, width=350) # entry url
-        MainFrame.e_url.grid(row=0, column=1, padx=0, pady=15)
+        
+        MainFrame.e_url = ctk.CTkEntry(self, width=485) # entry url
+        MainFrame.e_url.grid(row=0, column=1, padx=(0, 25), pady=15)
+        
         self.b_search = ctk.CTkButton(self, text="Search", width=140, command=search, font=font_title, image=img_search, compound="left")
         self.b_search.grid(row=0, column=2, padx=(20, 20), pady=15)
-        self.img_thumb = ctk.CTkLabel(self, image=my_image, text="")  # display video cover with a CTkLabel
-        self.img_thumb.grid(row=2, column=0, rowspan=5, columnspan=2, padx=(20, 20), pady=(15, 30))
+        
+        MainFrame.img_thumb = ctk.CTkLabel(self, image=None, text="Cover", anchor=ctk.NW, width=575, height=325)  # display video thumb with a CTkLabel
+        MainFrame.img_thumb.grid(row=2, column=0, rowspan=5, columnspan=2, padx=(20, 20), pady=(15, 30))
+        
         self.b_download = ctk.CTkButton(self, text="Baixar", width=140, command=None, font=font_title, height=50, image=img_download, compound="left")
         self.b_download.grid(row=6, column=2, padx=(20, 20), pady=(15, 30))
 
-        # widgets with video data
-        MainFrame.l_title = ctk.CTkLabel(self, text="Título:", font=font_title)
+        # Widgets with video data
+        MainFrame.l_title = ctk.CTkLabel(self, text="Título:", font=font_label)
         MainFrame.l_title.grid(row=1, column=0, columnspan=3, padx=20, pady=0, stick="nw")
-        self.l_title = ctk.CTkLabel(self, text="Canal: ", font=font_label)
-        self.l_title.grid(row=2, column=2, padx=20, pady=15, stick="nw")
-        self.l_title = ctk.CTkLabel(self, text="Views: ", font=font_label)
-        self.l_title.grid(row=3, column=2, padx=20, pady=15, stick="nw")
-        self.l_title = ctk.CTkLabel(self, text="Data: ", font=font_label)
-        self.l_title.grid(row=4, column=2, padx=20, pady=15, stick="nw")
-        self.l_title = ctk.CTkLabel(self, text="Duração: ", font=font_label)
-        self.l_title.grid(row=5, column=2, padx=20, pady=15, stick="nw")
+        
+        MainFrame.l_channel = ctk.CTkLabel(self, text="Canal: ", font=font_label)
+        MainFrame.l_channel.grid(row=2, column=2, padx=20, pady=15, stick="nw")
+        
+        MainFrame.l_views = ctk.CTkLabel(self, text="Views: ", font=font_label)
+        MainFrame.l_views.grid(row=3, column=2, padx=20, pady=15, stick="nw")
+        
+        MainFrame.l_date = ctk.CTkLabel(self, text="Data: ", font=font_label)
+        MainFrame.l_date.grid(row=4, column=2, padx=20, pady=15, stick="nw")
+        
+        MainFrame.l_duration = ctk.CTkLabel(self, text="Duração: ", font=font_label)
+        MainFrame.l_duration.grid(row=5, column=2, padx=20, pady=15, stick="nw")
+
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         #self.geometry("680x580")
         self.title("YouTube Downloader App")
-        self.grid_rowconfigure(0, weight=1)  # configure grid system
+
+        # configure grid system
+        self.grid_rowconfigure(0, weight=1)  
         self.grid_columnconfigure(0, weight=1)
 
         # Add frames to App        
